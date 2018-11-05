58d4a1a19c246d947f805aec40fd5f909c775394
wangqifan <1694675518@qq.com>
2018-11-05 13:20:02 +0800

    2015.1

---
+++ 2015/2015.1.cpp
@@ -0,0 +1,23 @@
+#include<string>
+#include<iostream>
+using namespace std;
+bool ishuiwen(string str)
+{
+    if(str.length()<=1)return true;
+    int left=0;
+    int right=str.length()-1;
+    while(left<right)
+    {
+        if(str[left]!=str[right])return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+int main()
+{
+	string str;
+	cin>>str;
+	cout<<ishuiwen(str); 
+    return 0;
+}
