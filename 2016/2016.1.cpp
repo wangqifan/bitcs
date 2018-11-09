bfb243caf7303881120abca4bfc1ce9ae0590cba
wangqifan <1694675518@qq.com>
2018-11-09 23:11:39 +0800

    2016

---
+++ 2016/2016.1.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+struct ListNode{
+      int val;
+      ListNode *next;
+      ListNode(int _val)
+      {
+          val=_val;
+          next=NULL;
+      }
+}; 
+void reverse(ListNode *head)//head指向头节点
+{
+    if(head==NULL)return;
+    if(head->next==NULL)return;
+    ListNode *node=head->next;
+    while(node->next!=NULL)//头插法
+    {
+        ListNode *temp=node->next;
+        node->next=temp->next;
+        temp->next=head->next;
+        head->next=temp;
+    }
+}
+void visited(ListNode *head)
+{
+     while(head->next!=NULL)
+     {
+         cout<<head->next->val<<" ";
+         head=head->next;
+     }
+     cout<<endl;
+}
+int main()
+{
+     ListNode *head=new ListNode(0);
+     ListNode *tail=head;
+     for(int i=0;i<10;i++)
+     {
+        ListNode *temp=new ListNode(i+1);
+        tail->next=temp;
+        tail=temp;
+     }
+     visited(head);
+     reverse(head);
+     visited(head);
+}
